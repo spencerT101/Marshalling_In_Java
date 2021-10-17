08bee8e1015f7dd505fc683ba8ec3e501d6e028e
Spencer <Spencer.Taber@yahoo.com>
2021-10-17 17:53:22 +0100

    committing changes

--- src/main/java/com/example/iwservice/shippingaddress/ShippingAddress.java
+++ src/main/java/com/example/iwservice/shippingaddress/ShippingAddress.java
@@ -6,9 +6,12 @@ public class ShippingAddress {
 
 
     private String ShippingAddressLine1;
+    private String ShippingAddressLine2;
 
-    public ShippingAddress(String shippingAddressLine1) {
+
+    public ShippingAddress(String shippingAddressLine1, String shippingAddressLine2) {
         this.ShippingAddressLine1 = shippingAddressLine1;
+        this.ShippingAddressLine2 = shippingAddressLine2;
 
     }
     public ShippingAddress(){
@@ -22,6 +25,14 @@ public class ShippingAddress {
 
     public void setShippingAddressLine1(String shippingAddressLine1) {
         this.ShippingAddressLine1 = shippingAddressLine1;
+    }
+
+    public String getShippingAddressLine2() {
+        return ShippingAddressLine2;
+    }
+
+    public void setShippingAddressLine2(String shippingAddressLine2) {
+        ShippingAddressLine2 = shippingAddressLine2;
     }
 }
 
--- src/test/java/ShippingAddressTest.java
+++ src/test/java/ShippingAddressTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class ShippingAddressTest {
+import com.example.iwservice.shippingaddress.ShippingAddress;
+import org.junit.Before;
+
+public class ShippingAddressTest {
+
+    private ShippingAddress shippingAddress0;
+
+    @Before
+    public void setup(){
+        shippingAddress0 = new ShippingAddress("123 Fake St", "Fake Building");
+    }
+
+    
 }
