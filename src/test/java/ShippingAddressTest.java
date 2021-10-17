68cac448b0ef303dd64aacd73f79f09e286d3d52
Spencer <Spencer.Taber@yahoo.com>
2021-10-17 18:04:39 +0100

    get and set address line 1 unit tests passed

--- src/test/java/ShippingAddressTest.java
+++ src/test/java/ShippingAddressTest.java
@@ -1,14 +1,32 @@
 import com.example.iwservice.shippingaddress.ShippingAddress;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ShippingAddressTest {
 
     private ShippingAddress shippingAddress0;
+    private String newAddressLine1;
 
     @Before
     public void setup(){
         shippingAddress0 = new ShippingAddress("123 Fake St", "Fake Building");
     }
 
+    @Test
+    public void canGetShippingAddressLine1(){
+        assertEquals("123 Fake St", shippingAddress0.getShippingAddressLine1());
+    }
+
+    @Test
+    public void setShippingAddress0(){
+        newAddressLine1 = "321 Fake St";
+        shippingAddress0.setShippingAddressLine1(newAddressLine1);
+        assertEquals("321 Fake St", shippingAddress0.getShippingAddressLine1());
+    }
+
+    @Test
     
+
 }
