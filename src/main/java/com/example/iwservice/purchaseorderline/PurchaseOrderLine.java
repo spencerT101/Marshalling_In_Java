9ee78ef863c1a8a59c377529ef7b08f13f85310b
Spencer <Spencer.Taber@yahoo.com>
2021-10-18 23:13:41 +0100

    get ItemID unit test passed

--- src/main/java/com/example/iwservice/purchaseorderline/PurchaseOrderLine.java
+++ src/main/java/com/example/iwservice/purchaseorderline/PurchaseOrderLine.java
@@ -2,4 +2,13 @@ package com.example.iwservice.purchaseorderline;
 
 public class PurchaseOrderLine {
 
+    private String itemID;
+
+    public PurchaseOrderLine(String itemID){
+        this.itemID = itemID;
+    }
+
+    public String getItemID(){
+        return itemID;
+    }
 }
--- src/test/java/PurchaseOrderLineTest.java
+++ src/test/java/PurchaseOrderLineTest.java
@@ -2,15 +2,19 @@ import com.example.iwservice.purchaseorderline.PurchaseOrderLine;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PurchaseOrderLineTest {
 
     private PurchaseOrderLine purchaseOrderLine0;
 
     @Before
     public void setUp(){
-        purchaseOrderLine0 = new PurchaseOrderLine();
+        purchaseOrderLine0 = new PurchaseOrderLine("123456");
     }
 
     @Test
-    public void canGetItemId
+    public void canGetItemID(){
+        assertEquals("123456", purchaseOrderLine0.getItemID());
+    }
 }
