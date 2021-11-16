02c8e7bdd3719ba02c8d342207140280d82790e2
Spencer <Spencer.Taber@yahoo.com>
2021-11-16 21:42:29 +0000

    committing changes

--- src/main/java/com/example/iwservice/SecurityConfiguration.java
+++ src/main/java/com/example/iwservice/SecurityConfiguration.java
@@ -1,29 +1,32 @@
 package com.example.iwservice;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password("Admin1234").roles("ADMIN")
-                .and()
-                .withUser("user").password("User1234").roles("USER");
+                .withUser("admin")
+                .password("Admin1234")
+                .roles("ADMIN");
+//                .and()
+//                .withUser("user").password("User1234").roles("USER");
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
-                .authorizeRequests()
-                .anyRequest().authenticated()
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
                 .and()
                 .httpBasic();
     }
