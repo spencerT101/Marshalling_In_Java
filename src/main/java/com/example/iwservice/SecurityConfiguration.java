a3ffd26f94ff248c74b35b045f8ad07789456247
Spencer <Spencer.Taber@yahoo.com>
2021-11-16 20:24:08 +0000

    added Maven dependency for security

---
+++ src/main/java/com/example/iwservice/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.iwservice;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
