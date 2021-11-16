caabbd2c5505746709c8076d2dc740ab97001b50
Spencer <Spencer.Taber@yahoo.com>
2021-11-16 20:46:25 +0000

    added http security method function

--- src/main/java/com/example/iwservice/SecurityConfiguration.java
+++ src/main/java/com/example/iwservice/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package com.example.iwservice;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,6 +18,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("admin").password("Admin1234").roles("ADMIN")
                 .and()
                 .withUser("user").password("User1234").roles("USER");
-
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
     }
 }
