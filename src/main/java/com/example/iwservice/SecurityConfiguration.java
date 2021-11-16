f54e36ce708f7ffd2a6569c998c7b57796821e6c
Spencer <Spencer.Taber@yahoo.com>
2021-11-16 20:34:12 +0000

    working on function for security config

--- src/main/java/com/example/iwservice/SecurityConfiguration.java
+++ src/main/java/com/example/iwservice/SecurityConfiguration.java
@@ -2,10 +2,18 @@ package com.example.iwservice;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("user1234").roles("USER");
+
+    }
 }
