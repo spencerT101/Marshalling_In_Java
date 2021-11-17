04c8131582d400c2c479e5fdbce2c464f3d8b5b1
Spencer <Spencer.Taber@yahoo.com>
2021-11-17 22:59:49 +0000

    xml file successfully validating against schema, will merge to main

--- src/main/java/com/example/iwservice/purchaseorder/PurchaseOrder.java
+++ src/main/java/com/example/iwservice/purchaseorder/PurchaseOrder.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 
-@XmlRootElement
+@XmlRootElement(name = "PurchaseOrder")
 public class PurchaseOrder {
 
 
