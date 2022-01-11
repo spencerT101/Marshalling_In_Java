31d3bfd0413420336ef2edb667c89a96170dcdf0
Spencer <Spencer.Taber@yahoo.com>
2022-01-11 21:00:51 +0000

    comitting erorr controller

---
+++ src/main/java/com/example/iwservice/controllers/CustomErrorController.java
@@ -0,0 +1,41 @@
+package com.example.iwservice.controllers;
+
+import com.example.iwservice.purchaseorder.PurchaseOrder;
+import org.apache.tomcat.jni.File;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+
+//    public String getErrorPath(){
+//
+//        return "src/main/resources";
+//    }
+
+    @RequestMapping("src/resources")
+    public String handleError(HttpServletRequest request, Model model){
+
+        String PATH = "src/main/resources/sample404ErrorOutput.json";
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if(status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()){
+                return PATH;
+            }
+        }
+
+            return PATH;
+
+    }
+}
