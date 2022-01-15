8c96f462711bb01e7993e163fe2f94b55ee29c38
Spencer <Spencer.Taber@yahoo.com>
2022-01-15 16:01:36 +0000

    committing changes

--- src/main/java/com/example/iwservice/controllers/PurchaseOrderControllerAdvice.java
+++ src/main/java/com/example/iwservice/controllers/PurchaseOrderControllerAdvice.java
@@ -6,28 +6,31 @@ import com.example.iwservice.purchaseorder.PurchaseOrder;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+
 @ControllerAdvice()
 public class PurchaseOrderControllerAdvice implements ErrorController {
 
-    PurchaseOrderController purchaseOrderController = new PurchaseOrderController();
+//    PurchaseOrderController purchaseOrderController = new PurchaseOrderController();
+//    PurchaseOrder purchaseOrder;
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<?> handleNoHandlerFoundException(HttpServletRequest request, Throwable ex) throws IOException {
         HttpStatus status = getStatus(request);
-       if(status == HttpStatus.NOT_FOUND){
+//      if(status == HttpStatus.NOT_FOUND){
         String PATH = "src/main/resources/sample404ErrorOutput.json";
-        String content = new String ( Files.readAllBytes( Paths.get(PATH) ) );
+        String content = new String (Files.readAllBytes(Paths.get(PATH)));
         return new ResponseEntity<>(content, status);
-   }
-        return null;
-    }
+        }
+//      return ResponseEntity<>(HttpStatus.FOUND);
+//    }
 
     private HttpStatus getStatus(HttpServletRequest request) {
         Integer code = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
