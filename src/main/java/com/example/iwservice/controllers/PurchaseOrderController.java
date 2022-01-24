2d685a40cd1f7d7c008f1fa6af3caff9c48ab688
Spencer <Spencer.Taber@yahoo.com>
2022-01-24 22:48:11 +0000

    added 201 response status annotation to PurchaseOrderController

--- src/main/java/com/example/iwservice/controllers/PurchaseOrderController.java
+++ src/main/java/com/example/iwservice/controllers/PurchaseOrderController.java
@@ -3,6 +3,7 @@ import com.example.iwservice.purchaseorder.PurchaseOrder;
 import com.example.iwservice.orderservice.OrderService;
 import com.example.iwservice.schemavalidation.SchemaValidation;
 import com.example.iwservice.xmlorderunmarshaller.XmlOrderUnmarshaller;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.xml.sax.SAXException;
@@ -17,7 +18,7 @@ public class PurchaseOrderController {
     private SchemaValidation schemaValidation = new SchemaValidation();
     private XmlOrderUnmarshaller xmlOrderUnmarshaller = new XmlOrderUnmarshaller();
 
-
+    @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(value = "/order/json", method = RequestMethod.POST)
     public PurchaseOrder getPurchaseOrderJson(@RequestBody() String purchaseOrderRaw) throws JAXBException, IOException, SAXException {
         schemaValidation.validate( purchaseOrderRaw, "src/main/resources/schema.xsd");
@@ -26,6 +27,7 @@ public class PurchaseOrderController {
         return purchaseOrder;
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(value = "/order/xml", method = {RequestMethod.POST}, produces = {MediaType.APPLICATION_XML_VALUE})
     public PurchaseOrder getPurchaseOrderXml(@RequestBody() String purchaseOrderRaw) throws JAXBException, IOException, SAXException {
         schemaValidation.validate( purchaseOrderRaw, "src/main/resources/schema.xsd");
